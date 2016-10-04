511cd8d0e0e3bb5f4d31607b760fcb91e7d06aef
Rogiel <rogiel2009@gmail.com>
2016-10-03 21:55:03 -0300

    mapa criado

--- app/src/main/java/orbus/example/computeiro/orbus/MapsActivity.java
+++ app/src/main/java/orbus/example/computeiro/orbus/MapsActivity.java
@@ -1,14 +1,29 @@
 package orbus.example.computeiro.orbus;
 
+
+import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+
+//localizacao
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+
+
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,9 +53,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
+
+
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(-22.007373, -47.894752);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("USP"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15));
+
+
     }
 }
